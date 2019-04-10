fefa7151df21561eace6a755914190ede80ae510
Asoum Alagha <asoumalagha@gmail.com>
2019-04-10 17:20:55 +0200

    Done with the navigation, minor bug appeared - table headers disappears

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,47 @@
 
-let myTable = document.querySelector('#mytable')
+let f = 1;
 
-for (let i=0;i<61;i++) {
-  $.getJSON('https://swapi.co/api/planets/'+i, function(response){
-      let name = response['name'];
-      let diameter = response['diameter'];
-      let climate = response['climate'];
-      let terrain = response['terrain'];
-      let surface_water = response['surface_water']+'%';
-      if (surface_water === 'unknown%'){
-          surface_water = '-'
-      }
-      let population = response['population'];
+
+let prevBtn = document.querySelector('#prev');
+let nextBtn = document.querySelector('#next');
+
+
+function displayPage(){
+    let myTable = document.querySelector('#mytable');
+    $.getJSON('https://swapi.co/api/planets/?page=' + f, function(response){
+    myTable.innerHTML = ``;
+    let result = response.results;
+    for (let i=0;i<result.length;i++) {
+        let name = result[i].name;
+        let diameter = result[i].diameter;
+        let climate = result[i].climate;
+        let terrain = result[i].terrain;
+        let surface_water = result[i].surface_water;
+        let population = result[i].population;
       myTable.insertAdjacentHTML('beforeend',`<tr><td>${name}</td><td>${diameter}</td><td>${climate}</td><td>${terrain}</td><td>${surface_water}</td><td>${population}</td></tr>`);
+
+
+        }
     });
 }
 
+displayPage();
 
 
+prevBtn.addEventListener('click', function(event){
+    if (f <= 1){
+        f = 1;
+    }
+    f -= 1;
+    displayPage();
+
+});
+
+nextBtn.addEventListener('click', function(event){
+    if (f >= 6){
+        f = 6;
+    }
+    f += 1;
+    displayPage();
+
+});
